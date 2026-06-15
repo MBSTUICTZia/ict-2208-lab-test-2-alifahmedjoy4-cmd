74bce8c7059f3bd44d94491b52ead9955669940a
Alif Ahmed Joy <alifahmedjoy4@gmail.com>
2026-06-15 11:00:55 +0600

    Update Circle.java

--- shapes/Circle.java
+++ shapes/Circle.java
@@ -1,42 +1,32 @@
+package shapes;
 
 public class Circle extends Shape {
 
     private double radius;
 
-    // Constructor
-    public Circle(String color, boolean filled,
-                  double radius) {
-
+    public Circle(String color, boolean filled, double radius) {
         super(color, filled);
-
         this.radius = radius;
     }
 
-    // Area
     @Override
     public double getArea() {
         return Math.PI * radius * radius;
     }
 
-    // Perimeter
     @Override
     public double getPerimeter() {
         return 2 * Math.PI * radius;
     }
 
-    // toString
     @Override
     public String toString() {
-
         return "[ Circle ]\n" +
                super.toString() + "\n" +
                "Radius: " + radius + "\n" +
-               String.format("Area         : %.2f", getArea()) + "\n" +
+               String.format("Area         : %.2f", getArea())      + "\n" +
                String.format("Circumference: %.2f", getPerimeter());
     }
 
-    // Getter
-    public double getRadius() {
-        return radius;
-    }
+    public double getRadius() { return radius; }
 }
